8197517872e0ab09f3b1e49269d9f5fae463adfd
lenatsi <tsitukhinaalena@gmail.com>
2021-05-22 19:08:41 +0200

    Init

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,17 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TopMenuComponent } from './top-menu/top-menu.component';
+import { FooterComponent } from './footer/footer.component';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [			
+    AppComponent,
+      TopMenuComponent,
+      FooterComponent,
+      HomeComponent
+   ],
   imports: [
     BrowserModule,
     AppRoutingModule
